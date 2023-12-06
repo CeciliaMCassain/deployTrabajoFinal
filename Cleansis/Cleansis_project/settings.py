135fb501c03e8236582956551117f6e7720fb5b8
CeciliaMCassain <ceciliacassain@gmail.com>
2023-12-06 08:49:10 -0300

    mysql config added

--- Cleansis/Cleansis_project/settings.py
+++ Cleansis/Cleansis_project/settings.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = "Cleansis_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'CeciliaMCassain$Cleansis',
+            'USER': 'CeciliaMCassain',
+            'PASSWORD': 'pirulo1234',
+            'HOST': 'CeciliaMCassain.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
 }
 
 
