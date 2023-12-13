c39025253f5e1fec2b552cf6e3af6650519118a6
CeciliaMCassain <ceciliacassain@gmail.com>
2023-12-13 00:26:45 -0300

    Revert "test carrusel"
    This reverts commit 251dc5d3618e5f12e9d1367f23a1e6f128531c42.

--- static/js/carrusel.js
+++ static/js/carrusel.js
@@ -1,12 +1,15 @@
-const puntos = document.querySelectorAll('.punto');
-const carrouselImages = document.querySelectorAll('.img');
+const punto = document.querySelectorAll('.punto')
+const carrouselImages = document.querySelectorAll(".img")
 
-puntos.forEach((punto, index) => {
-  punto.addEventListener('click', () => {
-    puntos.forEach((punto) => punto.classList.remove('activo'));
-    carrouselImages.forEach((image) => image.classList.remove('select'));
+punto.forEach( (cadaPunto , i)=> {
+    
+    punto[i].addEventListener('click',()=>{
+        punto.forEach((cadaPunto , i)=>{
+            punto[i].classList.remove('activo')
+            carrouselImages[i].classList.remove("select")
+        })
+    punto[i].classList.add('activo')
+    carrouselImages[i].classList.add('select')
 
-    punto.classList.add('activo');
-    carrouselImages[index].classList.add('select');
-  });
-});
+    })
+})
